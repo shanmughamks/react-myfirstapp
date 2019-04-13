f61fd9c40375fb93d24d8c34da2e15e7d9b37725
ShanmughamKS <ks_shan@hotmail.com>
2019-04-13 17:27:34 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -19,8 +19,7 @@ class App extends Component {
           >
             Learn React
           </a>
-          <p>This is my first React App</p>
-          <button>Save</button>
+          <p>This is my first React App</p>          
         </header>
       </div>
     );
